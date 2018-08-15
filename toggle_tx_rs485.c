7d0544c451c02dbb35cb4c708ac6e1bfc7a95627
Iqram Ali <35267920+iqramali@users.noreply.github.com>
2018-08-15 11:18:28 +0000

    Update and rename tx_time_enable_rs485.c to toggle_tx_rs485.c

--- toggle_tx_rs485.c
+++ toggle_tx_rs485.c
@@ -20,7 +20,25 @@ void rs485_mode(int on) {
 	}
 }
 
+/* Disbale the receiver while transmit, since transmit and receive lines are shared in a RS485 two wire bus, 
+it might be useful to disable the receiver during a transmit to avoid reading back currently send data. 
+For that case use c_cflag in struct termios.
+*/
+void rs485_receiver(int on) {
+	struct termios options;
+
+	tcgetattr(fd, &options);
+	if(on)
+		options.c_cflag |= CREAD;
+	else
+		options.c_cflag &= ~CREAD;
+	tcsetattr(fd, TCSANOW, &options);
+	fcntl(fd, F_SETFL, FNDELAY);
+}
+
+
 int main (void) {
-  rs485_mode(1);
+   rs485_mode(1);
+   rs485_receiver(1);
   return 0;
 }
